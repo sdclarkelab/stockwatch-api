ac4a3d873a1b5186c042a242fd8f54b8be3bc0ce
Stephen Clarke <44792415+sdclarkelab@users.noreply.github.com>
2020-12-13 20:13:28 -0500

    fix stock name not showing issue. If cache is empty, get stock info from api

--- stock/services.py
+++ stock/services.py
@@ -1,4 +1,3 @@
-import json
 from datetime import datetime
 
 import redis
@@ -177,7 +176,7 @@ def get_stock_names_from_cache():
     :return:
     """
     try:
-        return json.loads(r.get('stock_names'))
+        return jamstockex_api_service.get_stocks_infos()
     except Exception as get_stock_names_from_cache_error:
         print(get_stock_names_from_cache_error)
         return []
