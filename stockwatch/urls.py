5a9f715cbd81cc7998018290c1ad56c0b03c7aea
Stephen Clarke <44792415+sdclarkelab@users.noreply.github.com>
2021-01-03 13:50:04 -0500

    Created custom token view to return investor info on token endpoint

--- stockwatch/urls.py
+++ stockwatch/urls.py
@@ -13,13 +13,48 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import json
+
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
-from .views import page_not_found
+from django.utils.decorators import method_decorator
+from django.views.decorators.debug import sensitive_post_parameters
+from oauth2_provider.models import get_access_token_model
+from oauth2_provider.signals import app_authorized
+from oauth2_provider.views.base import TokenView
+
 from stock import views as stock_views
+from .views import page_not_found
+
+
+class CustomTokenView(TokenView):
+    @method_decorator(sensitive_post_parameters("password"))
+    def post(self, request, *args, **kwargs):
+        url, headers, body, status = self.create_token_response(request)
+        if status == 200:
+            body = json.loads(body)
+            access_token = body.get("access_token")
+            if access_token is not None:
+                token = get_access_token_model().objects.get(
+                    token=access_token)
+                app_authorized.send(
+                    sender=self, request=request,
+                    token=token)
+                body['member'] = {
+                    'id': token.user.id,
+                    'username': token.user.username
+                }
+                body = json.dumps(body)
+        response = HttpResponse(content=body, status=status)
+        for k, v in headers.items():
+            response[k] = v
+        return response
+
 
 urlpatterns = [
     path('api/v1/stockwatch_admin/', admin.site.urls),
+    path('api/v1/stockwatch_admin/o/token/', CustomTokenView.as_view(), name='token'),
     path('api/v1/stockwatch_admin/o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('api/v1/investor/', include('investor.urls')),
     path('api/v1/stockNames/', stock_views.stock_names, name='stock_names'),
