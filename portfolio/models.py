e752f0495a880896a3282e53b873a3d134398d4f
Stephen Clarke <44792415+sdclarkelab@users.noreply.github.com>
2021-01-23 22:03:26 -0500

    Added is_default attribute to portfolio table

--- portfolio/models.py
+++ portfolio/models.py
@@ -10,3 +10,4 @@ class Portfolio(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='portfolio_user')
     status = models.ForeignKey(PortfolioStatus, on_delete=models.CASCADE, related_name='portfolio_status')
     name = models.TextField(max_length=20, default="My Portfolio")
+    is_default = models.BooleanField(null=True, default=False)
